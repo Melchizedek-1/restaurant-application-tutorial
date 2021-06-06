4b1687f050e8c5b53dfce511460b78a84d399f89
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-06-06 14:28:49 +0300

    solved authenticator problem

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -6,13 +6,15 @@ exports.authenticateJWT = (req, res, next) => {
     
     if (!token) {
         return res.status(401).json({
-            errorMessage: 'No token. Authoriation denied',
+            errorMessage: 'No token. Authorization denied',
         });
     }
 
     try {
         const decoded = jwt.verify(token, jwtSecret);
         req.user = decoded.user;
+
+        next();
     } catch (err) {
         console.log('jwt error: ', err);
         res.status(401).json({
