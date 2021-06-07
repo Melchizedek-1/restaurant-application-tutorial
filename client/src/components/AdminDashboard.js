77de8f834502db274e4eb8a82ed86f0559cc15a7
Melchizedek-1 <31878736+Melchizedek-1@users.noreply.github.com>
2021-06-07 10:12:24 +0300

    formdata object and createproduct api function

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { createCategory, getCategories } from '../api/category';
+import { createProduct } from '../api/product';
 import isEmpty from 'validator/lib/isEmpty';
 import { showErrorMsg, showSuccessMsg } from '../helpers/message';
 import { showLoading } from '../helpers/loading';
@@ -11,6 +12,23 @@ const AdminDashboard = () => {
     const [errorMsg, setErrorMsg] = useState('');
     const [successMsg, setSucessMsg] = useState('');
     const [loading, setLoading] = useState('');
+    const [productData, setProductData] = useState({
+        productImage: null,
+        productName: '',
+        productDesc: '',
+        productPrice: '',
+        productCategory: '',
+        productQty: '',
+    });
+
+    const {
+        productImage,
+        productName,
+        productDesc,
+        productPrice,
+        productCategory,
+        productQty,
+    } = productData;
 
     useEffect(() => {
         loadCategories();
@@ -61,6 +79,50 @@ const AdminDashboard = () => {
 
         }
         
+    };
+
+    const handleProductImage = (evt) => {
+        console.log(evt.target.files[0]);
+        setProductData({
+            ...productData,
+            [evt.target.name]: evt.target.files[0],
+        });
+    };
+
+    const handleProductChange = (evt) => {
+        setProductData({
+            ...productData,
+            [evt.target.name]: evt.target.value,
+        });
+    }
+
+    const handleProductSubmit = (evt) => {
+        evt.preventDefault();
+        if (productImage === null) {
+            setErrorMsg('Please select an Image')
+        } else if (isEmpty(productName) || isEmpty(productDesc) || isEmpty(productPrice)) {
+            setErrorMsg('All fields are required');
+        } else if (isEmpty(productCategory)) {
+            setErrorMsg('Please select a category');
+        } else if (isEmpty(productQty)) {
+            setErrorMsg('Please select a quantity');
+        } else {
+            let formData = new FormData();
+            formData.append('productImage', productImage);
+            formData.append('productName', productName);
+            formData.append('productDesc', productDesc);
+            formData.append('productPrice', productPrice);
+            formData.append('productCategory', productCategory);
+            formData.append('productQty', productQty);
+
+            createProduct(formData)
+                .then(response => {
+                    console.log('Server response', response);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
     };
 
     const showHeader = () => (
@@ -144,7 +206,7 @@ const AdminDashboard = () => {
         <div id='addFoodModal' className='modal' onClick={handleMessages}>
             <div className='modal-dialog modal-dialog-centered modal-lg'>
                 <div className='modal-content'>
-                    <form onSubmit={handleCategorySubmit}>
+                    <form onSubmit={handleProductSubmit}>
                         <div className='modal-header bg-warning text-white'>
                             <h5 className='modal-title'>Add Food</h5>
                             <button className='close' data-dismiss='modal'>
@@ -159,28 +221,28 @@ const AdminDashboard = () => {
                             {loading ? (<div className='text-center'>{showLoading()}</div> ) : (
                                 <Fragment>
                                     <div className='custom-file mb-2'>
-                                        <input type='file' className='custom-file-input' />
+                                        <input type='file' className='custom-file-input' name='productImage' onChange={handleProductImage} />
                                         <label className='custom-file-label'>
                                             Choose a file
                                         </label>
                                     </div>
                                     <div className='form-group'>
                                         <label className='text-secondary'>Name</label>
-                                        <input type='text' className='form-control' />
+                                        <input type='text' className='form-control' name='productName' value={productName} onChange={handleProductChange} />
                                     </div>
                                     <div className='form-group'>
                                         <label className='text-secondary'>Description</label>   
-                                        <textarea className='form-control' rows='3'></textarea>
+                                        <textarea className='form-control' rows='3' name='productDesc' value={productDesc} onChange={handleProductChange}></textarea>
                                     </div>
                                     <div className='form-group'>
                                         <label className='text-secondary'>Price</label>
-                                        <input type='text' className='form-control' />
+                                        <input type='text' className='form-control' name='productPrice' value={productPrice} onChange={handleProductChange} />
                                     </div>
                                     <div className='form-row'>
                                         <div className='form-group col-md-6'>
                                             <label className='text-secondary'>Category</label>
-                                            <select className='custom-select mr-sm-2'>
-                                                <option>Choose one...</option>
+                                            <select className='custom-select mr-sm-2' name='productCategory' onChange={handleProductChange}>
+                                                <option value=''>Choose one...</option>
                                                 {categories && categories.map((c) => (
                                                     <option key={c._id} value={c._id}>{c.category}</option>
                                                 ))}
@@ -190,7 +252,7 @@ const AdminDashboard = () => {
                                             <label className='text-secondary'>
                                                 Quantity
                                             </label>
-                                            <input type='number' className='form-control' min='0' max='1000' />
+                                            <input type='number' className='form-control' min='0' max='1000' name='productQty' value={productQty} onChange={handleProductChange} />
                                         </div>
                                     </div>
                                 </Fragment>
@@ -210,6 +272,7 @@ const AdminDashboard = () => {
 
     return (
         <section>
+            {JSON.stringify(productData)}
             {showHeader()}
             {showActionBtns()}
             {showCategoryModal()}
